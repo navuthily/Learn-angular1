04671fb3bcdd9a15e5125016ac7be59cc1f23ea2
Na Vu <vuthilyna304@gmail.com>
2024-06-01 00:34:13 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent],
+  // templateUrl: './app.component.html',
+  // styleUrl: './app.component.css',
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/house.svg"
+          alt="logo"
+          aria-hidden="true"
+          width="20"
+          height="20"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
 })
 export class AppComponent {
   title = 'a1';
+  hoverColorVar = '#999999';
+  primaryColor = '#777777';
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  // templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+  `,
+})
+export class HomeComponent {}
